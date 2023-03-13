f87fa76c31a85e7cd661ffa5af2b1cc1951bf81d
aliveplanarium <gitkivaylo@mail.ru>
2023-03-13 14:13:39 +0300

    Commit important добавлено много всего

--- GroupProject-106/Program.cs
+++ GroupProject-106/Program.cs
@@ -1,4 +1,4 @@
-namespace GroupProject_106
+namespace WinFormsApp1
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace GroupProject_106
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1(""));
         }
     }
 }
